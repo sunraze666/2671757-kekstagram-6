f29abeaabd689751ae26d47df44fce4fc71c62c9
DmitryH <Dimopsss@yahoo.com>
2025-09-20 12:14:51 +0500

    add functions

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+function lenStr(str, maxlength)  {
+  if (str.length <= maxlength) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+// Cтрока короче 20 символов
+console.log(lenStr('проверяемая строка', 20))
+// Длина строки ровно 18 символов
+console.log(lenStr('проверяемая строка', 18))
+// Строка длиннее 10 символов
+console.log(lenStr('проверяемая строка', 10))
+
+
+function palimdrome(str) {
+  str = str.replaceAll(' ', '');
+  str = str.toLowerCase();
+  return str === str.split('').reverse().join('');
+}
+
+console.log(palimdrome('Лёша на полке клопа нашёл ')) // true
